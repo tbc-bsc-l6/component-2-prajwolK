1b71505f0a4d0add86c9219a6f5ca47e9d90e7e2
prajwolK <kprajowal22@tbc.edu.np>
2025-01-02 17:08:54 +0545

    admin user seed

--- restro/database/seeders/Admin.php
+++ restro/database/seeders/Admin.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class Admin extends Seeder
@@ -12,6 +15,13 @@ class Admin extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => 1,
+        ]);
     }
 }
