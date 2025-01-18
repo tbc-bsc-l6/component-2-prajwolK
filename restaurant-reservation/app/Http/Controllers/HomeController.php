6e107495f046049b344f9260f098a835d6ea27e5
prajwolK <kprajowal22@tbc.edu.np>
2025-01-18 12:01:27 +0545

    User and Admin authentication adjusted using jetstream

---
+++ restaurant-reservation/app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id())
+        {
+            $user_type = Auth()->user()->user_type;
+            if($user_type=='user')
+            {
+                return view('dashboard');
+            }
+            else{
+                return view('admin.index');
+            }
+        }
+    }
+}
--- restaurant-reservation/routes/web.php
+++ restaurant-reservation/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home',[HomeController::class,'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
